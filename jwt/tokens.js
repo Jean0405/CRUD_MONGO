14cc8b2057ebdf2b812f5a441c12e2e10babff28
Jean0405 <jeanpierrea0405@gmail.com>
2023-08-17 00:02:00 -0500

    feat: :lock: Se crea el módulo de token
    Se crea el módulo encargado de generar y validar los tokens

--- jwt/tokens.js
+++ jwt/tokens.js
@@ -1,58 +1,86 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
 import { Router } from "express";
-import { classToPlain, plainToClass } from "class-transformer";
+import { classToPlain, plainToInstance } from "class-transformer";
 import { SignJWT, jwtVerify } from "jose";
 /*Hay que importar los DTO de cada tabla
 para que cuando se envien al params este ya este importado en este archivo*/
 import { Sucursal } from "../storage/controllers/sucursal.js";
+import { Automovil } from "../storage/controllers/automovil.js";
+import { Alquiler } from "../storage/controllers/alquiler.js";
+import { Clientes } from "../storage/controllers/clientes.js";
+import { Empleados } from "../storage/controllers/empleado.js";
+import { Reserva } from "../storage/controllers/reserva.js";
 
 dotenv.config();
 const GENERATE_TOKEN = Router();
 const VERIFY_TOKEN = Router();
 
+const instanceDTO = (className) => {
+  const classMap = {
+    sucursal: Sucursal,
+    automovil: Automovil,
+    alquiler: Alquiler,
+    cliente: Clientes,
+    empleado: Empleados,
+    reserva: Reserva,
+  };
+
+  const Class = classMap[className];
+  return Class
+    ? plainToInstance(Class, {}, { ignoreDecorators: true })
+    : undefined;
+};
+
 GENERATE_TOKEN.use("/:collection", async (req, res) => {
   try {
-    let instance = plainToClass(
-      eval(req.params.collection),
-      {},
-      { ignoreDecorators: true }
-    );
+    const collectionName = req.params.collection;
+    const instance = instanceDTO(collectionName);
+
+    if (!instance)
+      return res.status(404).send({
+        status: 404,
+        message: "ERROR: La colección no ha sido encontrada",
+      });
     const encoder = new TextEncoder();
     const jwtConstructor = new SignJWT(
       Object.assign({}, classToPlain(instance))
     );
-    const jwt = await jwtConstructor
+
+    req.data = await jwtConstructor
       .setProtectedHeader({
         alg: "HS256",
         typ: "JWT",
       })
       .setIssuedAt()
-      .setExpirationTime("30m")
-      .sign(encoder.encode(process.env.JWT_KEY));
-    req.data = jwt;
-    res.status(201).send({ status: 201, message: jwt });
+      .setExpirationTime("1h")
+      .sign(encoder.encode(process.env.PRIVATE_KEY));
+    res.status(201).send({ status: 201, message: req.data });
   } catch (error) {
-    res.status(404).send({ status: 404, message: error.message });
+    res
+      .status(404)
+      .send({ status: 404, message: "ERROR: Token solicitado no es valido" });
   }
 });
 
 VERIFY_TOKEN.use("/", async (req, res) => {
   const { authorization } = req.headers;
   if (!authorization)
-    return res.status(400).send({ status: 400, message: "Token no incluido" });
-
+    return res
+      .status(400)
+      .send({ status: 400, message: "Token no asignado en el header" });
   try {
     const encoder = new TextEncoder();
     const jwtData = await jwtVerify(
       authorization,
-      encoder.encode(process.env.JWT_KEY)
+      encoder.encode(process.env.PRIVATE_KEY)
     );
     req.data = jwtData;
-    console.log(req.data);
     next();
   } catch (error) {
-    res.status(498).send({ status: 489, message: "Token caducado" });
+    res
+      .status(498)
+      .send({ status: 498, message: "ERROR: Token no valido o expirado" });
   }
 });
 
